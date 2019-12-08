391452a55ef48aad96fbcf30cf02aea94656990d
Marc-Andre Lanciault <malanciault@gmail.com>
2019-12-07 19:04:58 -0500

    Threel_lang

---
+++ src/Threel_lang.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Malanciault\Threelci;
+
+class Threel_lang extends \CI_lang {
+  /**
+   * Language line
+   *
+   * Fetches a single line of text from the language array
+   *
+   * @param string  $line   Language line key
+   * @param bool  $log_errors Whether to log an error message if the line is not found
+   * @return  string  Translation
+   */
+  public function line($line, $log_errors = TRUE)
+  {
+    $value = isset($this->language[$line]) ? $this->language[$line] : FALSE;
+
+    return $value;
+  }
+}
